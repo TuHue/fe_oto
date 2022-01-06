e6f8063a20e49936a1fed32208393ad8153aa1a1
TuHue <94226385+TuHue@users.noreply.github.com>
2022-01-06 08:29:23 +0700

    first commit 3

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,9 @@
+let headerMiniNav = document.querySelector('#header-mini__nav');
 let btnBar = document.querySelector('#btn--fa-bars');
 let btnTimes = document.querySelector('#btn--fa-times');
-let headerMiniNav = document.querySelector('#header-mini__nav');
+let btnTestDriver = document.querySelector('#btn--test-driver');
+let from1 = document.querySelector('#home-1__form-1');
+let from1Close = document.querySelector('#from1__close');
 
 btnBar.addEventListener('click', function() {
     headerMiniNav.style.display = 'block';
@@ -12,3 +15,9 @@ btnTimes.addEventListener('click', function() {
     btnTimes.style.display = 'none';
     btnBar.style.display = 'block';
 });
+btnTestDriver.addEventListener('click', function() {
+    from1.style.display = 'block';
+})
+from1Close.addEventListener('click', function() {
+    from1.style.display = 'none'
+})

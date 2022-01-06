1ee3283540fd0daa3eb982008097eed3e783dcfa
TuHue <94226385+TuHue@users.noreply.github.com>
2022-01-06 08:39:44 +0700

    first commit 4

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,8 +2,11 @@ let headerMiniNav = document.querySelector('#header-mini__nav');
 let btnBar = document.querySelector('#btn--fa-bars');
 let btnTimes = document.querySelector('#btn--fa-times');
 let btnTestDriver = document.querySelector('#btn--test-driver');
+let btnAdvise = document.querySelector('#btn--advise');
 let from1 = document.querySelector('#home-1__form-1');
+let from2 = document.querySelector('#home-1__form-2');
 let from1Close = document.querySelector('#from1__close');
+let from2Close = document.querySelector('#from2__close');
 
 btnBar.addEventListener('click', function() {
     headerMiniNav.style.display = 'block';
@@ -16,8 +19,16 @@ btnTimes.addEventListener('click', function() {
     btnBar.style.display = 'block';
 });
 btnTestDriver.addEventListener('click', function() {
+    from2.style.display = 'none'
     from1.style.display = 'block';
+})
+btnAdvise.addEventListener('click', function() {
+    from1.style.display = 'none'
+    from2.style.display = 'block';
 })
 from1Close.addEventListener('click', function() {
     from1.style.display = 'none'
+})
+from2Close.addEventListener('click', function() {
+    from2.style.display = 'none'
 })

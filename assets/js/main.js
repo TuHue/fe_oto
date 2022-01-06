a4e19d047a41ba142bd411abb779b7f4c89aa16e
TuHue <94226385+TuHue@users.noreply.github.com>
2022-01-06 15:57:52 +0700

    first commit 10

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,34 +1,44 @@
-let headerMiniNav = document.querySelector('#header-mini__nav');
-let btnBar = document.querySelector('#btn--fa-bars');
-let btnTimes = document.querySelector('#btn--fa-times');
-let btnTestDriver = document.querySelector('#btn--test-driver');
-let btnAdvise = document.querySelector('#btn--advise');
-let from1 = document.querySelector('#home-1__form-1');
-let from2 = document.querySelector('#home-1__form-2');
-let from1Close = document.querySelector('#from1__close');
-let from2Close = document.querySelector('#from2__close');
+let headerMiniNav = document.querySelector("#header-mini__nav");
+let btnBar = document.querySelector("#btn--fa-bars");
+let btnTimes = document.querySelector("#btn--fa-times");
+let btnTestDriver = document.querySelector("#btn--test-driver");
+let btnAdvise = document.querySelector("#btn--advise");
+let from1 = document.querySelector("#home-1__form-1");
+let from2 = document.querySelector("#home-1__form-2");
+let from1Close = document.querySelector("#from1__close");
+let from2Close = document.querySelector("#from2__close");
+let tagText = document.querySelectorAll(".tag--text");
 
-btnBar.addEventListener('click', function() {
-    headerMiniNav.style.display = 'block';
-    btnBar.style.display = 'none';
-    btnTimes.style.display = 'block';
+btnBar.addEventListener("click", function() {
+    headerMiniNav.style.display = "block";
+    btnBar.style.display = "none";
+    btnTimes.style.display = "block";
 });
-btnTimes.addEventListener('click', function() {
-    headerMiniNav.style.display = 'none';
-    btnTimes.style.display = 'none';
-    btnBar.style.display = 'block';
+btnTimes.addEventListener("click", function() {
+    headerMiniNav.style.display = "none";
+    btnTimes.style.display = "none";
+    btnBar.style.display = "block";
 });
-btnTestDriver.addEventListener('click', function() {
-    from2.style.display = 'none'
-    from1.style.display = 'block';
-})
-btnAdvise.addEventListener('click', function() {
-    from1.style.display = 'none'
-    from2.style.display = 'block';
-})
-from1Close.addEventListener('click', function() {
-    from1.style.display = 'none'
-})
-from2Close.addEventListener('click', function() {
-    from2.style.display = 'none'
-})
+btnTestDriver.addEventListener("click", function() {
+    from2.style.display = "none";
+    from1.style.display = "block";
+});
+btnAdvise.addEventListener("click", function() {
+    from1.style.display = "none";
+    from2.style.display = "block";
+});
+from1Close.addEventListener("click", function() {
+    from1.style.display = "none";
+});
+from2Close.addEventListener("click", function() {
+    from2.style.display = "none";
+});
+
+for (let i = 0; i < tagText.length; i++) {
+    tagText[i].addEventListener("click", function() {
+        for (let i = 0; i < tagText.length; i++) {
+            tagText[i].classList.remove("tag--text--active")
+        };
+        this.classList.add("tag--text--active")
+    });
+}
